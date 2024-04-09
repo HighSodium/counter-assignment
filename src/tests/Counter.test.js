653bd94a2b8a3a5ab606bae7540fad2f94b9d406
Spooky <thepallitta@gmail.com>
2024-04-09 17:24:33 -0400

    Unit tests for Counter app

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,39 @@
-// import necessary react testing library helpers here
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/';
 // import the Counter component here
+import Counter from '../components/Counter';
+
 
 beforeEach(() => {
   // Render the Counter component here
+  render(<Counter/>)
 })
 
 test('renders counter message', () => {
   // Complete the unit test below based on the objective in the line above
+  expect(screen.getByText('Counter')).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
   // Complete the unit test below based on the objective in the line above
+  expect(screen.getByTestId('count')).toHaveTextContent('0');
 });
 
 test('clicking + increments the count', () => {
   // Complete the unit test below based on the objective in the line above
+  const addButton = screen.getByText('+');
+  fireEvent.click(addButton);
+  expect(screen.getByTestId('count')).toHaveTextContent('1');
+  fireEvent.click(addButton);
+  expect(screen.getByTestId('count')).toHaveTextContent('2');
 });
 
 test('clicking - decrements the count', () => {
   // Complete the unit test below based on the objective in the line above
+  const minusButton = screen.getByText('-');
+  fireEvent.click(minusButton);
+  expect(screen.getByTestId('count')).toHaveTextContent('-1');
+  fireEvent.click(minusButton);
+  expect(screen.getByTestId('count')).toHaveTextContent('-2');
 });
